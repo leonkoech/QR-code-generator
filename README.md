217e0ef7eee53ead2a5c757f6b7bc326376ce5cb
Leon Kipkoech <leonkipkip@gmail.com>
2021-09-21 17:32:38 -0400

    change qr code color

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 A mini python script that creates QR codes for you. The only variable it takes is your string.
 
 ## Requirements
-1. qrcode package which can be found [here](https://pypi.org/project/qrcode/)
-2. python 3.6+
+1. [qrcode package](https://pypi.org/project/qrcode/)
+2. [pillow](https://pypi.org/project/Pillow/)
+3. python 3.6+
+
+## How To
+1. The program asks for an input text
+2. select whether you want the background of the qr code to be transparent or not
+2a. If you want a tranpsarent background the code proceeds
+2b. if you want to set a background you'll follow input prompts for the RGB values
+3. Select whether you want the color for the qr code to be transparent or not
+3a. If you want a transparent qr code, it will be generated and stored under processed.png
+3b. If you want to set the color or the qr code, you'll have to input the RGB values by following the prompts 
--- main.py
+++ main.py
@@ -1,8 +1,73 @@
+from PIL import Image
 import qrcode
+def backgroundColor(r,g,b,a):
+    image=Image.open('unprocessed.png')
+    # transform image to RGBA
+    image = image.convert('RGBA')
+    print(image)
+    print(image.mode)
+    # print(image.getdata()[2])
+    newImage = []
+    for item in image.getdata():
+        if item[:3] == (255,255,255):
+            # enter the new color here
+            # choose whether transparent or other ccolors
+            if a==0:
+                newImage.append((255,255,255,0))
+            else:
+                newImage.append((r,g,b))
+        
+        else:
+            newImage.append(item)
+    image.putdata(newImage)
+    image.save('unprocessed.png')
+
+def codeColor(r,g,b,a):
+    image=Image.open('unprocessed.png')
+    # transform image to RGBA
+    image = image.convert('RGBA')
+    print(image)
+    print(image.mode)
+    # print(image.getdata()[2])
+    newImage = []
+    for item in image.getdata():
+        if item[:3] == (0,0,0):
+            # enter the new color here
+            # choose whether transparent or other colors
+            if a==0:
+                newImage.append((255,255,255,0))
+            else:
+                newImage.append((r,g,b))
+        
+        else:
+            newImage.append(item)
+    image.putdata(newImage)
+    image.save('processed.png')
+
+
+
 def main():
-    link="https://fnutechclub.web.app/"
-    img = qrcode.make(link)
+    inputtext=input("enter the text you want to turn into a qr code: ")
+    img = qrcode.make(inputtext)
     type(img)  
-    img.save("fnutechclub.png")
+    img.save("unprocessed.png")
+    bgcolor = input("is your background transparent: (y/N)")
+    if bgcolor == '' or bgcolor == 'y' or bgcolor == 'Y':
+        backgroundColor(255,255,255,0)
+    else:
+        bgr = input("enter the R value of RGB: ")
+        bgg = input("enter the B value of RGB: ")
+        bgb = input("enter the A value of RGB: ")
+        backgroundColor(int(bgr),int(bgg),int(bgb),1)
+    codecolor = input("is your qr code transparent: (y/N)")
+    if codecolor == '' or codecolor == 'y' or codecolor == 'Y':
+        codeColor(255,255,255,0)
+    else:
+        ccr = input("enter the R value of RGB: ")
+        ccg = input("enter the B value of RGB: ")
+        ccb = input("enter the A value of RGB: ")
+        codeColor(int(ccr),int(ccg),int(ccb),1)
+  
+   
 if __name__ == '__main__':
     main()
